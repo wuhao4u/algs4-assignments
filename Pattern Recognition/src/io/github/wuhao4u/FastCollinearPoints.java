87a861d7f4b89d62a5e17c15ff821fd971caed97
wuhao4u <wuhao4u@gmail.com>
2017-06-13 17:37:31 -0400

    PA3: first passable version, still need to fix Fast dups

--- Pattern Recognition/src/io/github/wuhao4u/FastCollinearPoints.java
+++ Pattern Recognition/src/io/github/wuhao4u/FastCollinearPoints.java
@@ -5,7 +5,6 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * Created by wuhao on 2017-06-10.
@@ -13,36 +12,28 @@ import java.util.HashMap;
 public class FastCollinearPoints {
     private int segCount;
     private LineSegment[] segs;
-    private HashMap<Point, Point> lineSegMap;
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
+        Point[] slopesToPoint = points.clone();
+        Arrays.sort(slopesToPoint);
+
         if (points == null) throw new NullPointerException();
-        else if (hasDuplicatedPoints(points)) {
+        else if (hasDuplicatedPoints(slopesToPoint)) {
             throw new IllegalArgumentException("Contains repeated points");
-        } else if (points.length < 4) {
-            throw new IllegalArgumentException("Less than 4 points.");
         }
 
         segCount = 0;
         segs = new LineSegment[8];
-        lineSegMap = new HashMap<>();
 
-        Point[] slopesToPoint = points.clone();
         for (int i = 0; i < points.length; ++i) {
             Arrays.sort(slopesToPoint, 0, slopesToPoint.length, points[i].slopeOrder());
 
             // find collinear (exclude itself)
-//            System.out.printf("--- print slopes to %d point ---\n", i);
-//            System.out.println(points[i]);
-//            System.out.println("-------------------------------");
-
             int colCount = 0;
             for (int j = 0; j < slopesToPoint.length; ++j) {
                 if (points[i].compareTo(slopesToPoint[j]) != 0) {
                     // this is not the point p itself
-//                    System.out.println(points[i].slopeTo(slopesToPoint[j]));
-//                    System.out.print("colCount: " + colCount + '\n');
 
                     if (points[i].slopeOrder().compare(slopesToPoint[j], slopesToPoint[j - 1]) == 0) {
                         // this point q1 and the previous point q2 has same slope to point p
@@ -61,12 +52,10 @@ public class FastCollinearPoints {
                             Arrays.sort(pointsInLine, 0, pointsInLine.length);
 
                             // if not already being added, then add this new segment
-                            // OLDTODO: here maybe wrong. we want the corner ones
+                            // TODO: here maybe wrong. we want the corner ones
                             if (!isInSegments(pointsInLine[0], pointsInLine[pointsInLine.length - 1])) {
                                 segs[segCount++] = new LineSegment(pointsInLine[0], pointsInLine[pointsInLine.length - 1]);
-                                lineSegMap.put(pointsInLine[0], pointsInLine[pointsInLine.length - 1]);
                             }
-
                         }
                     }
                     else {
@@ -82,7 +71,7 @@ public class FastCollinearPoints {
                             Arrays.sort(pointsInLine, 0, pointsInLine.length);
 
                             // if not already being added, then add this new segment
-                            // OLDTODO: here maybe wrong. we want the corner ones
+                            // TODO: here maybe wrong. we want the corner ones
                             segs[segCount++] = new LineSegment(pointsInLine[0], pointsInLine[pointsInLine.length - 1]);
                         }
 
@@ -95,19 +84,7 @@ public class FastCollinearPoints {
     }
 
     private boolean isInSegments(Point p0, Point p1) {
-        if (p0 == null || p1 == null) throw new NullPointerException("p0 or p1 is null.");
-
-        if (lineSegMap.isEmpty() || lineSegMap == null) {
-            return false;
-        }
-
-        if (lineSegMap.containsKey(p0) && lineSegMap.get(p0).compareTo(p1) == 0) {
-            return true;
-        } else if (lineSegMap.containsKey(p1) && lineSegMap.get(p1).compareTo(p0) == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
     private LineSegment[] resizeSegments(int capacity) {
@@ -118,12 +95,10 @@ public class FastCollinearPoints {
         return nSegs;
     }
 
+    // the points needs to be sorted
     private boolean hasDuplicatedPoints(Point[] points) {
-        for (int i = 0; i < points.length; ++i) {
-            if (points == null) throw new NullPointerException("some point in the array is null.");
-            for (int j = i + 1; j < points.length; ++j) {
-                if (points[i].compareTo(points[j]) == 0) return true;
-            }
+        for (int i = 1; i < points.length; ++i) {
+            if (points[i].equals(points[i-1])) return true;
         }
         return false;
     }
