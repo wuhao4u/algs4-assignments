b1e5066c3f1adcb1ad1ffb209b1d1c9d69f2c7f6
wuhao4u <wuhao4u@gmail.com>
2017-06-15 23:49:27 -0400

    PA4: starting on solver

--- 8puzzle/src/io/github/wuhao4u/Solver.java
+++ 8puzzle/src/io/github/wuhao4u/Solver.java
@@ -6,22 +6,52 @@ import edu.princeton.cs.algs4.Queue;
 /**
  * Created by wuhao on 2017-06-14.
  */
+/*
+TODO:
+1. detect unsolvable puzzles
+2. game tree
+ */
+
 public final class Solver {
     private final Board tiles;
-    private final MinPQ<SearchNode> sol;
-    private int movesMade;
+    private final MinPQ<SearchNode> openSet;
+    private final MinPQ<SearchNode> closedSet;
+    private final int movesMade;
 
     private final class SearchNode {
-        Board b;
-        int moveMade;
-        SearchNode prev;
+        // a board
+        Board tiles;
+
+        // the number of moves made to reach the board
+        // cost from the start node to this node
+        int gScore;
+
+        // (optional?) cost from start to the goal (gScore + hamming/manhattan)
+        // manhattan + moves
+        int fScore;
+
+        // previous search node
+        SearchNode cameFrom;
+
+        public SearchNode(Board initBoard, int gs, int fs, SearchNode cf) {
+            this.tiles = initBoard;
+            this.gScore = gs;
+            this.fScore = fs;
+            this.cameFrom = cf;
+        }
     }
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) throw new NullPointerException("Missing initial board.");
+
+        // initialize data structures
         this.tiles = initial;
-        this.sol = new MinPQ<>();
+        this.openSet = new MinPQ<>();
+        this.closedSet = new MinPQ<>();
+        this.movesMade = 0;
+
+        SearchNode start = new SearchNode();
     }
 
     // is the initial board solvable?
@@ -31,7 +61,10 @@ public final class Solver {
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return movesMade;
+        if (isSolvable())
+            return movesMade;
+        else
+            return -1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
@@ -42,5 +75,6 @@ public final class Solver {
     }
 
     // solve a slider puzzle (given below)
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 }
