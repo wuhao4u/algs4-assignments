e4c7219997714ea709f1aa0e9482ca57fda738bd
wuhao4u <wuhao4u@gmail.com>
2017-06-22 22:17:51 -0400

    PA5: wrote range and nearest functions, not sure if they are correct

--- Kd-Trees/src/io/github/wuhao4u/KdTree.java
+++ Kd-Trees/src/io/github/wuhao4u/KdTree.java
@@ -78,7 +78,6 @@ public class KdTree {
     // hv: orientation of the new node
     private Node put(Node n, Point2D p, boolean isVertical, RectHV parentRect) {
         // root node has no parent node
-        // TODO: the rectangle
         if (n == null) return new Node(p, parentRect, null, null);
 
         int cmp;
@@ -102,7 +101,7 @@ public class KdTree {
             }
             n.lb = put(n.lb, p, !isVertical, rhv);
         }
-        if (cmp > 0) {
+        if (cmp >= 0) {
             if (isVertical) {
                 // set Xmin
                 rhv = new RectHV(n.val.x(), parentRect.ymin(), parentRect.xmax(), parentRect.ymax());
@@ -121,7 +120,6 @@ public class KdTree {
 
     // draw all points to standard draw
     public void draw() {
-        // TODO: doing
         drawNode(mRoot, true);
     }
 
@@ -153,22 +151,88 @@ public class KdTree {
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
         // TODO
+        // if the query rectangle does not intersect the rectangle corresponding to a node,
+        // there is no need to explore that node (or its subtrees).
+        // A subtree is searched only if it might contain a point contained in the query rectangle.
         TreeSet ts = new TreeSet<Point2D>();
+
+        range(mRoot, rect, ts);
+
         return ts;
+    }
+
+    private void range(Node node, RectHV rect, TreeSet<Point2D> set) {
+        if (node == null || rect == null || set == null) return;
+
+        if (!rect.intersects(node.rect)) return;
+
+        if (rect.contains(node.val)) set.add(node.val);
+
+        range(node.lb, rect, set);
+        range(node.rt, rect, set);
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         // TODO
-        return p;
+        // start at the root and recursively search in both subtrees
+        Point2D nearestPoint = nearest(mRoot, p, null);
+
+        return nearestPoint;
+    }
+
+    private Point2D nearest(Node node, Point2D target, Point2D closestSoFar) {
+        if (target == null) throw new NullPointerException("missing target");
+        if (node == null) return closestSoFar;
+        if (closestSoFar == null) {
+            closestSoFar = node.val;
+        }
+
+        // if the closest point discovered so far is closer than the distance between the query point
+        // and the rectangle corresponding to a node, there is no need to explore that node (or its subtrees).
+
+        if (target.distanceTo(closestSoFar) < target.distanceTo(node.val)) {
+            // That is, a node is searched only if it might contain a point
+            // that is closer than the best one found so far.
+            return closestSoFar;
+        } else if (target.distanceTo(node.val) < target.distanceTo(closestSoFar)) {
+            // if current node's point is closer to the argument one, replace it
+            closestSoFar = node.val;
+        }
+
+        // The effectiveness of the pruning rule depends on quickly finding a nearby point.
+        // To do this, organize your recursive method so that when there are two possible subtrees to go down,
+        // you always choose the subtree that is on the same side of the splitting line
+        // as the query point as the first subtree to explore—the closest point found
+
+        // while exploring the first subtree may enable pruning of the second subtree.
+
+        if (node.lb != null && node.rt != null) {
+            Point2D closeSideRes, farSideRes;
+            if (target.distanceTo(node.lb.val) < target.distanceTo(node.rt.val)) {
+                // left-below side is closer to the targeting point
+                closeSideRes = nearest(node.lb, target, closestSoFar);
+                farSideRes = nearest(node.rt, target, closestSoFar);
+            } else {
+                // right-top side is closer to the targeting point
+                closeSideRes = nearest(node.rt, target, closestSoFar);
+                farSideRes = nearest(node.lb, target, closestSoFar);
+            }
+
+            closestSoFar = (target.distanceTo(closeSideRes) < target.distanceTo(farSideRes)) ? closeSideRes : farSideRes;
+        } else if (node.lb != null) {
+            closestSoFar = nearest(node.lb, target, closestSoFar);
+        } else if (node.rt != null) {
+            closestSoFar = nearest(node.rt, target, closestSoFar);
+        }
+
+        return closestSoFar;
     }
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
         String filename = args[0];
         In in = new In(filename);
-
-        StdDraw.enableDoubleBuffering();
 
         // initialize the data structures with N points from standard input
         KdTree kdtree = new KdTree();
@@ -180,17 +244,17 @@ public class KdTree {
             kdtree.insert(p);
         }
 
-        StdDraw.enableDoubleBuffering();
-        StdOut.println("---test draw---");
-        StdDraw.clear();
-        kdtree.draw();
-        StdDraw.show();
-        /*
+//        StdDraw.enableDoubleBuffering();
+//        StdOut.println("---test draw---");
+//        StdDraw.clear();
+//        kdtree.draw();
+//        StdDraw.show();
         Point2D p0, p05, p1;
         p0 = new Point2D(0, 0);
         p05 = new Point2D(0.5, 0.5);
         p1 = new Point2D(1., 1.);
 
+        /*
         StdOut.println(kdtree.contains(p0));
         StdOut.println(kdtree.contains(p05));
         StdOut.println(kdtree.contains(p1));
@@ -198,7 +262,22 @@ public class KdTree {
         StdOut.println(kdtree.contains(new Point2D(0.756544, 0.417366)));
         StdOut.println(kdtree.contains(new Point2D(0.785370, 0.652338)));
         StdOut.println(kdtree.contains(new Point2D(0.406360, 0.678100)));
-//        StdOut.println(kdtree.contains(new Point2D(0.147733,0.203388)));
+
+        // test range
+        StdOut.println("---test range---");
+        RectHV leftHalf = new RectHV(0, 0, 0.50, 1);
+        RectHV rightHalf = new RectHV(0.51, 0, 1, 1);
+
+        TreeSet<Point2D> leftHalfPoints = (TreeSet<Point2D>) kdtree.range(leftHalf);
+        TreeSet<Point2D> rightHalfPoints = (TreeSet<Point2D>) kdtree.range(rightHalf);
+
+        StdOut.println("Number of points in the left half: " + leftHalfPoints.size());
+        StdOut.println("Number of points in the right half: " + rightHalfPoints.size());
 */
+
+        // test nearest
+        StdOut.println("closest to point " + p0.toString() + "is point " + kdtree.nearest(p0).toString());
+        StdOut.println("closest to point " + p05.toString() + "is point " + kdtree.nearest(p05).toString());
+        StdOut.println("closest to point " + p1.toString() + "is point " + kdtree.nearest(p1).toString());
     }
 }
